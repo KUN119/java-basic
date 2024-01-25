afe1cc1789aa0be044b1d8684962b873c416807c
KUN119 <kjp2629@naver.com>
2024-01-25 15:03:08 +0900

    다형성, 완강

---
+++ src/poly/car1/CarMain1.java
@@ -0,0 +1,23 @@
+package poly.car1;
+
+public class CarMain1 {
+
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+
+        //차량 선택(k3)
+        K3Car k3Car = new K3Car();
+        driver.setCar(k3Car); //Car car = k3Car 부모는 자식을 담을 수 있다
+        driver.drive();
+
+        //차량 추가
+        Model3Car model3Car = new Model3Car();
+        driver.setCar(model3Car);
+        driver.drive();
+
+        NewCar newCar = new NewCar();
+        driver.setCar(newCar);
+        driver.drive();
+
+    }
+}
